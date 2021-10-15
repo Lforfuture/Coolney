f8f429d86f2500e95009aab44b5fca23d363566c
maylin <1078836421@qq.com>
2021-10-15 22:31:35 +0800

    配置部署到gitpage

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Coolney-website/'
+    : '/',
   lintOnSave: false,
   chainWebpack: (config) => {
     const dir = path.resolve(__dirname, "src/assets/icon");
